62495ef92d40c9a0f1992129e690c193b3aca44d
jesstoselli <jfamedeiros@gmail.com>
2020-09-26 02:10:26 -0300

    jogo finalizado

---
+++ script.js
@@ -0,0 +1,79 @@
+const dino = document.querySelector(".dino");
+const background = document.querySelector(".background");
+
+let isJumping = false;
+let isGameOver = false;
+let position = 0;
+
+function handleKeyUp(event) {
+  if (event.keyCode === 32) {
+    if (!isJumping) {
+      jump();
+    }
+  }
+}
+
+function jump() {
+  isJumping = true;
+
+  let upInterval = setInterval(() => {
+    if (position >= 150) {
+      // going down
+      clearInterval(upInterval);
+
+      let downInterval = setInterval(() => {
+        if (position <= 0) {
+          clearInterval(downInterval);
+          isJumping = false;
+        } else {
+          position -= 20;
+          dino.style.bottom = position + "px";
+        }
+      }, 20);
+    } else {
+      // going up
+      position += 20;
+      dino.style.bottom = position + "px";
+    }
+  }, 20);
+}
+
+function createCactus() {
+  const cactus = document.createElement("div");
+  let cactusPosition = 1000;
+  let randomTime = Math.random() * 6000;
+
+  cactus.classList.add("cactus");
+  cactus.style.left = 1000 + "px";
+  background.appendChild(cactus);
+
+  let leftTimer = setInterval(() => {
+    if (cactusPosition < -60) {
+      // left the screen
+      clearInterval(leftTimer);
+      background.removeChild(cactus);
+    } else if (cactusPosition > 0 && cactusPosition < 60 && position < 60) {
+      //game over
+      clearInterval(leftTimer);
+      isGameOver = true;
+      document.body.innerHTML = `
+      <div class='game-over'>
+      <h1>Game Over</h1>
+      <button type="button" onClick="refreshPage()">Restart!</button>
+      <div>
+      `;
+    } else {
+      cactusPosition -= 10;
+      cactus.style.left = cactusPosition + "px";
+    }
+  }, 20);
+
+  setTimeout(createCactus, randomTime);
+}
+
+function refreshPage() {
+  window.location.reload();
+}
+
+createCactus();
+document.addEventListener("keyup", handleKeyUp);
